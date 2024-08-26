bd4e41f01b984bbb9743e1414530804fd6a2d5b1
AleBj <alejandromarsico@gmail.com>
2024-08-26 11:13:49 -0300

    :zap: Fix URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   devtools: { enabled: false },
   app: {
-    baseURL:'/',
+    baseURL:'/action-days',
       //process.env.NODE_ENV === 'production' ? '/WIP/action-days/' : undefined,
     head: {
       title: 'UN SOTF Action Days',
