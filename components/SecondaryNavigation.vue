2da528501cc11b07a82df65bafc6125ab6a6a01e
Maryam TB <maryamtb.17@gmail.com>
2024-08-29 12:58:22 +0100

    qa tweaks

--- components/SecondaryNavigation.vue
+++ components/SecondaryNavigation.vue
@@ -106,7 +106,7 @@ export default {
       return s?.replaceAll("-", " ");
     },
     smoothScroll(target) {
-      target = target.replaceAll('&','')
+      target = target.replaceAll('&', '')
       document.querySelector(target).scrollIntoView({
         behavior: 'smooth',
         block: 'start'
@@ -121,35 +121,29 @@ export default {
 </script>
 
 <template>
-  <div
-    class="sticky top-[-1px] z-50 px-[16px] md:px-[60px] h-[64px] text-sm transition-all ease-in-out duration-300"
-    :class="currentNav === 'light' ? 'text-secondary' : 'text-primary'"
-  >
+  <div class="sticky top-[-1px] z-50 px-[16px] md:px-[60px] h-[64px] text-sm transition-all ease-in-out duration-300"
+    :class="currentNav === 'light' ? 'text-secondary' : 'text-primary'">
     <div class="h-full flex flex-row justify-between items-center ">
       <ul
         class="flex flex-row gap-60 h-full border-b transition-all ease-in-out duration-300 overflow-x-scroll	 md:overflow-x-auto"
-        :class="currentNav === 'light' ? 'border-secondary' : 'border-primary'"
-      >
-        <li
-          v-for="(item, index) in secondary_nav.data.links_menu"
-          :key="index"
+        :class="currentNav === 'light' ? 'border-secondary' : 'border-primary'">
+        <li v-for="(item, index) in secondary_nav.data.links_menu" :key="index"
           class="group h-full flex items-center justify-center relative cursor-pointer min-w-[120px] md:min-w-[auto]"
           :class="currentSection === item.link_url.url.substring(1) ? 'activeitem' : ''"
-          @click="smoothScroll(item.link_url.url)"
-        >
+          @click="smoothScroll(item.link_url.url)">
           <p
-            class="flex items-center before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[5px] before:rounded-lg before:bg-custom-radial before:transition-all before:duration-300 before:ease-out group-hover:before:w-full activeitem:before:w-full"
-          >
+            class="flex items-center before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[5px] before:rounded-lg before:bg-custom-radial before:transition-all before:duration-300 before:ease-out group-hover:before:w-full activeitem:before:w-full">
             {{ slugToString(item.link_title) }}
           </p>
         </li>
       </ul>
-      <button
-        class="hidden md:block text-xsm h-[40px] border rounded-full uppercase text-sm pl-30 pr-30 transition-all ease-in-out duration-300"
-        :class="currentNav === 'light' ? 'text-secondary border-secondary' : 'text-primary border-primary'"
-      >
-        Register
-      </button>
+      <NuxtLink :to="'/register'">
+        <button
+          class="hidden md:block text-xsm h-[40px] border rounded-full uppercase text-sm pl-30 pr-30 transition-all ease-in-out duration-300"
+          :class="currentNav === 'light' ? 'text-secondary border-secondary' : 'text-primary border-primary'">
+          Register
+        </button>
+      </NuxtLink>
     </div>
   </div>
   <div class="relative top-[-64px]">
@@ -158,7 +152,7 @@ export default {
 </template>
 
 <style scoped>
-  .activeitem p::before {
-    width: 100%;
-  }
+.activeitem p::before {
+  width: 100%;
+}
 </style>
--- slices/Announcements/index.vue
+++ slices/Announcements/index.vue
@@ -1,22 +1,22 @@
 <script setup>
-	import "@prismicio/client";
-	defineProps(getSliceComponentProps(["slice"]));
+import "@prismicio/client";
+defineProps(getSliceComponentProps(["slice"]));
 </script>
 
 <script>
-  import { Swiper, SwiperSlide } from 'swiper/vue';
-  import 'swiper/swiper-bundle.css';
-  import 'swiper/css/navigation'
-  import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
-  // import required modules
-  import { Pagination, Navigation } from 'swiper/modules';
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
 
-  import gsap from "gsap";
-  import ScrollTrigger from "gsap/ScrollTrigger";
-  import SplitText from "gsap/SplitText";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import SplitText from "gsap/SplitText";
 
-  export default {
+export default {
   components: {
     Swiper,
     SwiperSlide
@@ -50,29 +50,29 @@
         const titles = gsap.utils.toArray("[data-title]", el);
 
         if (titles) {
-            titles.forEach((title, index) => {
-							const duration = parseInt(title.dataset.duration) || 1.25;
-							const scrub = false || title.dataset.scrub;
+          titles.forEach((title, index) => {
+            const duration = parseInt(title.dataset.duration) || 1.25;
+            const scrub = false || title.dataset.scrub;
 
-							gsap.fromTo(titles, duration,
+            gsap.fromTo(titles, duration,
               {
                 yPercent: 150,
                 opacity: 0
-              },  
+              },
               {
-								ease: "expo.out",
-								yPercent: 0,
+                ease: "expo.out",
+                yPercent: 0,
                 opacity: 1,
-								stagger: 0.2,
-								scrollTrigger: {
-									trigger: el,
-									scrub: scrub,
-									start: "top 30%",
-									markers: false,
-								},
-							});
-						});
-          }
+                stagger: 0.2,
+                scrollTrigger: {
+                  trigger: el,
+                  scrub: scrub,
+                  start: "top 30%",
+                  markers: false,
+                },
+              });
+          });
+        }
       })
     }
   },
@@ -80,51 +80,25 @@
 </script>
 
 <template>
-  <section
-    :ref="setRef"
-    class="bg-primary px-40 py-[200px] text-white"
-    data-section="overview"
-    data-nav="light"
-  >
+  <section :ref="setRef" class="bg-primary px-40 py-[200px] text-white" data-section="overview" data-nav="light">
     <div>
       <div class="md:w-[961px] m-[auto] relative md:top-[50px] z-10 mb-[40px] md:mb-[0px]">
-        <h2
-          data-title
-          class="w-full uppercase text-titleSection_mb md:text-titleSection"
-        >
+        <h2 data-title class="w-full uppercase text-titleSection_mb md:text-titleSection">
           {{ slice.primary.title }}
         </h2>
-        <h2
-          data-title
-          class="w-full uppercase text-titleSection_mb md:text-titleSection md:ml-[180px]"
-        >
+        <h2 data-title class="w-full uppercase text-titleSection_mb md:text-titleSection md:ml-[180px]">
           {{ slice.primary.title_2 }}
         </h2>
       </div>
 
 
-      <Swiper
-        :slides-per-view="1"
-        :space-between="0"
-        :loop="true"
-        :navigation="{
-          prevEl: '.custom-prev',
-          nextEl: '.custom-next'
-        }"
-        :modules="modules"
-        class="relative overflow-hidden"
-      >
-        <SwiperSlide
-          v-for="(item, idx) in slice.primary.announcements"
-          :key="idx"
-          ref="items"
-          class=""
-        >
+      <Swiper :slides-per-view="1" :space-between="0" :loop="true" :navigation="{
+        prevEl: '.custom-prev',
+        nextEl: '.custom-next'
+      }" :modules="modules" class="relative overflow-hidden">
+        <SwiperSlide v-for="(item, idx) in slice.primary.announcements" :key="idx" ref="items" class="">
           <div class="flex flex-col md:flex-row items-center bg-[#195587]">
-            <PrismicImage
-              :field="item.image"
-              class="w-full aspect-square md:aspect-auto md:w-1/2"
-            />
+            <PrismicImage :field="item.image" class="w-full aspect-square md:aspect-auto md:w-1/2" />
             <div class="px-[24px] py-[40px] md:py-[0px] md:px-[65px] w-full md:w-1/2">
               <div class="mb-[20px] md:mb-[160px]">
                 <p class="uppercase font-bold pb-40 text-base">
@@ -132,69 +106,42 @@
                 </p>
                 <PrismicRichText :field="item.paragraph" />
               </div>
-              <div class="flex">
+              <div class="flex pt-80">
                 <button
-                  class="font-bold text-sm border border-[var(--burger-color)] z-[100] rounded-full uppercase text-white text-sm pl-30 pr-30 py-10"
-                >
+                  class="font-bold text-sm border border-[var(--burger-color)] z-[100] rounded-full uppercase text-white text-sm pl-30 pr-30 py-10">
                   View the Programme
                 </button>
+
+                <div class="md:absolute right-[40px] z-40 flex gap-[20px] justify-center md:justify-end">
+                  <div class="custom-prev cursor-pointer">
+                    <svg width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M29 19.6858L21.4286 27.2572L29 34.8286" stroke="#FFFFFF" stroke-width="2"
+                        stroke-miterlimit="13.33" stroke-linecap="square" />
+                      <circle opacity="0.5" cx="26.5" cy="26.5" r="26" transform="matrix(-1 0 0 1 53 0)"
+                        stroke="#FFFFFF" />
+                    </svg>
+                  </div>
+                  <div class="custom-next cursor-pointer">
+                    <svg width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M24 19.6858L31.5714 27.2572L24 34.8286" stroke="#FFFFFF" stroke-width="2"
+                        stroke-miterlimit="13.33" stroke-linecap="square" />
+                      <circle opacity="0.5" cx="26.5" cy="26.5" r="26" stroke="#FFFFFF" />
+                    </svg>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </SwiperSlide>
 
-        <div class="md:absolute bottom-[60px] right-[60px] z-40 flex gap-[20px] mt-[26px] justify-center md:justify-end">
-          <div class="custom-prev cursor-pointer">
-            <svg
-              width="53"
-              height="53"
-              viewBox="0 0 53 53"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M29 19.6858L21.4286 27.2572L29 34.8286"
-                stroke="#FFFFFF"
-                stroke-width="2"
-                stroke-miterlimit="13.33"
-                stroke-linecap="square"
-              />
-              <circle
-                opacity="0.5"
-                cx="26.5"
-                cy="26.5"
-                r="26"
-                transform="matrix(-1 0 0 1 53 0)"
-                stroke="#FFFFFF"
-              />
-            </svg>					
-          </div>
-          <div class="custom-next cursor-pointer">
-            <svg
-              width="53"
-              height="53"
-              viewBox="0 0 53 53"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M24 19.6858L31.5714 27.2572L24 34.8286"
-                stroke="#FFFFFF"
-                stroke-width="2"
-                stroke-miterlimit="13.33"
-                stroke-linecap="square"
-              />
-              <circle
-                opacity="0.5"
-                cx="26.5"
-                cy="26.5"
-                r="26"
-                stroke="#FFFFFF"
-              />
-            </svg>					
-          </div>
-        </div>
+
       </Swiper>
     </div>
   </section>
 </template>
+
+<style>
+.swiper-wrapper {
+  max-height: 620px !important;
+}
+</style>
