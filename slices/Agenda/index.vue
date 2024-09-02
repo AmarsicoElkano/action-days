feed920559f49da1f6a24b9dfa89f9cac5967e11
Maryam TB <maryamtb.17@gmail.com>
2024-09-02 18:36:48 +0100

    hover img backend

--- slices/Agenda/index.vue
+++ slices/Agenda/index.vue
@@ -60,18 +60,11 @@ export default {
 </script>
 
 <template>
-  <section
-    :ref="setRef"
-    class="w-full h-auto bg-secondary pb-[0px] md:pb-[213px]"
-    data-section="agenda"
-    data-nav="dark"
-  >
+  <section :ref="setRef" class="w-full h-auto bg-secondary pb-[0px] md:pb-[213px]" data-section="agenda"
+    data-nav="dark">
     <div class="w-full bg-secondary min-h-screen px-[16px] md:px-[0px]">
       <div class="flex flex-col md:flex-row justify-center pt-[100px] relative">
-        <PrismicImage
-          :field="slice.primary.bg_symbol"
-          class="absolute inset-0 w-full h-full z-0"
-        />
+        <PrismicImage :field="slice.primary.bg_symbol" class="absolute inset-0 w-full h-full z-0" />
 
         <div class="flex flex-col pb-[40px] md:pb-0px]">
           <p class="text-primary uppercase text-base w-[300px] font-bold">
@@ -84,28 +77,20 @@ export default {
         <div class="md:h-screen relative col-start-6 col-span-12">
           <div ref="masonry" class="masonry md:grid gap-[20px] relative">
             <div class="content text-primary text-download uppercase">
-              <div
-                class="py-20 md:p-20 md:grid grid-cols-2 md:w-[720px] gap-20"
-              >
-                <div
-                  v-for="(el, idx) in slice.primary.card_group"
-                  :key="idx"
+              <div class="py-20 md:p-20 md:grid grid-cols-2 md:w-[720px] gap-20">
+                <div v-for="(el, idx) in slice.primary.card_group" :key="idx"
                   class="p-[24px] mb-[20px] md:p-20 bg-white w-full max-w-[330px] h-[22rem] flex flex-col"
-                  :class="idx === 1 ? 'mt-20' : ''"
-                  data-item
-                >
-                  <PrismicLink
-                    :field="el?.link"
-                    class="group flex flex-col justify-between h-full"
-                  >
+                  :class="idx === 1 ? 'mt-20' : ''" data-item>
+                  <PrismicLink :field="el?.link" class="group flex flex-col justify-between h-full">
+
+                    <!-- Hover image -->
+                    <PrismicImage :field="el?.hover_image" />
+
                     <span class="block text-[18px]">
                       {{ el.subtitle }}
                     </span>
                     <div class="">
-                      <PrismicRichText
-                        :field="el.paragraph"
-                        :class="el.xl_font_size ? ' text-md' : ' text-base'"
-                      />
+                      <PrismicRichText :field="el.paragraph" :class="el.xl_font_size ? ' text-md' : ' text-base'" />
                     </div>
                   </PrismicLink>
                 </div>
