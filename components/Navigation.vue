01acf0333dce1ed3cb2709f797d4d28a782824e6
Maryam TB <maryamtb.17@gmail.com>
2024-09-04 15:37:57 +0100

    clean up

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -102,66 +102,6 @@ export default {
   },
 };
 </script>
-<!-- <template>
-  <div ref="nav"
-    class="max-h-[100vh] w-screen h-screen overflow-y-hidden fixed top-0 inset-0 bg-primary bg-radial-gradient flex items-center justify-center nav z-20 ">
-    <img class="absolute top-0 max-w-[900px] z-0" width="100%" height="auto" src="/public/img/menu-ellipse.svg" />
-    <nav class="w-full pl-70 pr-40 flex flex-row">
-      <LogoWhite class="absolute left-0 top-[10px] px-[16px] md:px-[60px]" />
-
-      <ul class="flex flex-col items-start absolute top-[20%] sm:top-[20%] md:top-[30%] lg:top-[35%]">
-        <li v-for="(item, index) in navigation.data.links_menu" :key="index"
-          class="text-white font-display uppercase text-lg md:text-md overflow-hidden relative py-20 md:py-10 last:border-b-[1px] last:border-white last:border-opacity-20 last:pb-30"
-          @click="toggle">
-          <div ref="navItems">
-            <PrismicLink
-              class="flex items-center border-t-[1px] border-white border-opacity-20 cursor-pointer min-w-[360px] sm:w-[450px] md:[650px] lg:w-[720px]"
-              :field="item.url_link">
-              <span class="font-light mt-40 ">0{{ index + 1 }}</span> <span
-                class="text-highlight sm:text-titleSection_mb md:text-titleSection_mb pl-20 pt-30 ">{{ slugToString(item.label_menu) }}</span>
-            </PrismicLink>
-          </div>
-        </li>
-      </ul>
-    </nav>
-    <div
-      class="flex flex-col px-0 md:px-80 sm:px-10 z-20 absolute top-[55%] sm:top-[31%] sm:right-[7%] md:top-[50%] lg:top-[56%] lg:right-[1%] gap-40 md:gap-10 lg:gap-10">
-      <PrismicLink :field="navigation.data.pact_link" class="max-w-[360px]">
-        <div class="w-[360px] h-[140px] py-8 pl-20 pr-6 bg-box-bg flex flex-row cursor-pointer justify-between">
-          <p
-            class="text-secondary uppercase w-[150px] pt-40 pl-40 md:pt-20 md:pl-10 font-bold sm:w-[110px] md:w-[140px] sm:leading-20 md:leading-10 z-30 ">
-            {{ navigation.data.pact_title }}
-          </p>
-          <img class="w-[53px] h-[53px] self-end mr-10 mb-5" src="/public/img/arrow-right-menu.svg">
-        </div>
-      </PrismicLink>
-      <PrismicLink :field="navigation.data.sof_link">
-        <div class="w-[360px] h-[140px] py-8 pl-20 pr-6 bg-box-bg flex flex-row cursor-pointer justify-between">
-          <p
-            class="text-secondary uppercase w-[150px] pt-40 pl-40 md:pt-20 md:pl-10 font-bold sm:w-[110px] md:w-[140px] sm:leading-20 md:leading-10 z-30 ">
-
-            {{ navigation.data.sof_title }}
-          </p>
-          <img class="w-[53px] h-[53px] self-end mr-10 mb-5" src="/public/img/arrow-right-menu.svg">
-        </div>
-      </PrismicLink>
-    </div>
-
-  </div>
-  <div class="flex flex-row gap-20">
-
-    <button ref="toggleNav"
-      class="w-[50px] h-[50px] flex flex-col border border-[var(--burger-color)] rounded-full items-center justify-center z-20 toggle-nav"
-      :class="{ active: isActive }" @click="toggle">
-      <div
-        class="w-[20px] h-[2px] bg-[var(--burger-color)] mx-auto origin-left transition-transform duration-700 ease-expo-out" />
-      <div
-        class="w-[20px] h-[2px] bg-[var(--burger-color)] my-[4px] mx-auto origin-left transition-transform duration-700 ease-expo-out" />
-      <div
-        class="w-[20px] h-[2px] bg-[var(--burger-color)] scale-x-50 origin-left transition-transform duration-700 ease-expo-out" />
-    </button>
-  </div>
-</template> -->
 <template>
   <div ref="nav"
     class="max-h-[100vh] w-screen max-h-screen !overflow-y-hidden fixed top-0 inset-0 bg-primary bg-radial-gradient flex flex-col justify-between nav z-20">
