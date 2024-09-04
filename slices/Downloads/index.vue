bb5bb8cb405a41d5f29c930683036065022b90b0
Maryam TB <maryamtb.17@gmail.com>
2024-09-04 15:31:03 +0100

    refactored downloads section

--- slices/Downloads/index.vue
+++ slices/Downloads/index.vue
@@ -118,7 +118,7 @@ export default {
 </script>
 
 <template>
-  <section id="documents" :ref="setRef" class="w-full min-h-screen w-full py-[140px] px-[16px] md:px-[60px]"
+  <section id="documents" :ref="setRef" class="w-full min-h-screen py-[140px] px-[16px] md:px-[60px]"
     :class="slice.primary.light_background_color ? 'bg-secondary' : 'bg-primary'"
     :data-nav="slice.primary.light_background_color ? 'dark' : 'light'" data-section="documents">
     <div class="max-w-[1300px] m-[auto]">
@@ -136,56 +136,57 @@ export default {
         </div>
       </div>
 
-      <div class="flex">
-        <div class="hidden md:block w-1/2" />
-        <div class="md:w-1/2 md:flex gap-[44px]">
-          <div v-for="(item, index) in slice.primary.documents" :key="index" class="mb-[40px] md:mb-[0px]" data-item>
-            <PrismicLink :field="item.download_url" class="group">
-              <figure class="relative">
-                <figcaption
-                  class="absolute z-10 w-full h-full p-[32px] md:px-20 md:py-30 text-primary flex flex-col justify-between">
-                  <p class="text-base_mb ">
-                    {{ item.subtitle }}
-                  </p>
-                  <h3 class="text-download uppercase text-md md:text-base">
-                    {{ item.title }}
-                  </h3>
-                </figcaption>
-                <PrismicImage class="z-0 w-full max-w-[297px] min-h-[343px] md:w-auto" :field="item.banner_image" />
-              </figure>
-              <div class="flex items-center justify-between mt-20">
-                <h4 class="uppercase tracking-[0.08em] text-md md:text-xsm"
-                  :class="slice.primary.light_background_color ? 'text-primary' : 'text-secondary'">
-                  {{ item.download_text }}
-                </h4>
-                <div
-                  class="w-[35px] h-[35px] flex flex-col border border-[var(--primary-color)] cursor-pointer rounded-full items-center justify-center relative before:absolute before:w-full before:h-full before:bg-primary before:inset-0 before:rounded-full before:scale-0 before:transition-transform group-hover:before:scale-90 before:duration-300 active:before:scale-100">
-                  <div class="inline-flex relative overflow-hidden">
-                    <div
-                      class="w-full h-full relative delay-150 group-hover:delay-0 transition-transform duration-200 expo-out group-hover:translate-y-full">
-                      <svg class="w-15 h-15" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 9.22754L8.37892 15.9028L1.75784 9.22754"
-                          :stroke="slice.primary.light_background_color ? 'var(--primary-color)' : 'var(--secondary-color)'"
-                          stroke-width="2" stroke-miterlimit="13.33" stroke-linecap="square" />
-                        <path d="M8.38281 14.9028L8.38281 1"
-                          :stroke="slice.primary.light_background_color ? 'var(--primary-color)' : 'var(--secondary-color)'"
-                          stroke-width="2" stroke-linecap="square" stroke-linejoin="round" />
-                      </svg>
-                    </div>
-                    <div
-                      class="w-full h-full absolute -top-full delay-0 group-hover:delay-150 transition-transform duration-200 expo-out group-hover:translate-y-full">
-                      <svg class="w-15 h-15" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 9.22754L8.37892 15.9028L1.75784 9.22754" stroke="var(--white)" stroke-width="2"
-                          stroke-miterlimit="13.33" stroke-linecap="square" />
-                        <path d="M8.38281 14.9028L8.38281 1" stroke="var(--white)" stroke-width="2"
-                          stroke-linecap="square" stroke-linejoin="round" />
-                      </svg>
-                    </div>
+      <div class="flex flex-wrap gap-20 justify-center md:justify-start"
+        :class="{ 'flex-wrap gap-20': slice.primary.documents.length > 2 }">
+        <div v-for="(item, index) in slice.primary.documents" :key="index" :class="{
+          'flex flex-row gap-20 mb-[40px]': slice.primary.documents.length > 2,
+          'w-1/2 md:w-1/2': slice.primary.documents.length <= 2
+        }" class="mb-[40px] md:mb-[0px]" data-item>
+          <PrismicLink :field="item.download_url" class="group">
+            <figure class="relative">
+              <figcaption
+                class="absolute z-10 max-w-[297px] h-full p-[32px] md:px-20 md:py-30 text-primary flex flex-col justify-between">
+                <p class="text-base_mb ">
+                  {{ item.subtitle }}
+                </p>
+                <h3 class="text-download uppercase text-md xs:text-highlight sm:text-highlight sm:md:text-base">
+                  {{ item.title }}
+                </h3>
+              </figcaption>
+              <PrismicImage class="z-0 w-full max-w-[297px] min-h-[343px] md:w-auto" :field="item.banner_image" />
+            </figure>
+            <div class="flex items-center justify-between mt-20 max-w-[297px]">
+              <h4 class="uppercase tracking-[0.08em] text-xsm"
+                :class="slice.primary.light_background_color ? 'text-primary' : 'text-secondary'">
+                {{ item.download_text }}
+              </h4>
+              <div
+                class="w-[35px] h-[35px] flex flex-col border border-[var(--primary-color)] cursor-pointer rounded-full items-center justify-center relative before:absolute before:w-full before:h-full before:bg-primary before:inset-0 before:rounded-full before:scale-0 before:transition-transform group-hover:before:scale-90 before:duration-300 active:before:scale-100">
+                <div class="inline-flex relative overflow-hidden">
+                  <div
+                    class="w-full h-full relative delay-150 group-hover:delay-0 transition-transform duration-200 expo-out group-hover:translate-y-full">
+                    <svg class="w-15 h-15" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15 9.22754L8.37892 15.9028L1.75784 9.22754"
+                        :stroke="slice.primary.light_background_color ? 'var(--primary-color)' : 'var(--secondary-color)'"
+                        stroke-width="2" stroke-miterlimit="13.33" stroke-linecap="square" />
+                      <path d="M8.38281 14.9028L8.38281 1"
+                        :stroke="slice.primary.light_background_color ? 'var(--primary-color)' : 'var(--secondary-color)'"
+                        stroke-width="2" stroke-linecap="square" stroke-linejoin="round" />
+                    </svg>
+                  </div>
+                  <div
+                    class="w-full h-full absolute -top-full delay-0 group-hover:delay-150 transition-transform duration-200 expo-out group-hover:translate-y-full">
+                    <svg class="w-15 h-15" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15 9.22754L8.37892 15.9028L1.75784 9.22754" stroke="var(--white)" stroke-width="2"
+                        stroke-miterlimit="13.33" stroke-linecap="square" />
+                      <path d="M8.38281 14.9028L8.38281 1" stroke="var(--white)" stroke-width="2"
+                        stroke-linecap="square" stroke-linejoin="round" />
+                    </svg>
                   </div>
                 </div>
               </div>
-            </PrismicLink>
-          </div>
+            </div>
+          </PrismicLink>
         </div>
       </div>
     </div>
