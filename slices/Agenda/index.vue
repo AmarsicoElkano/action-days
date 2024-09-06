a12d4b4e23b95d2ead32979a97ded706df797093
Maryam TB <maryamtb.17@gmail.com>
2024-09-06 20:50:44 +0100

    tiles dimensions tweaks more responsive

--- slices/Agenda/index.vue
+++ slices/Agenda/index.vue
@@ -60,18 +60,11 @@ export default {
 </script>
 
 <template>
-  <section
-    :ref="setRef"
-    class="w-full h-auto bg-secondary pb-[0px] md:pb-[213px]"
-    data-section="agenda"
-    data-nav="dark"
-  >
-    <div class="w-full bg-secondary min-h-screen px-[16px] md:px-[0px]">
-      <div class="flex flex-col md:flex-row justify-center pt-[100px] relative">
-        <PrismicImage
-          :field="slice.primary.bg_symbol"
-          class="absolute inset-0 w-full h-full z-0"
-        />
+  <section :ref="setRef" class="w-full h-auto bg-secondary pb-[0px] md:pb-[213px]" data-section="agenda"
+    data-nav="dark">
+    <div class="w-full h-full bg-secondary min-h-screen px-[16px] md:px-[0px] relative">
+      <div class="flex flex-col md:flex-row justify-center mx-auto pt-[200px] w-full relative">
+        <PrismicImage :field="slice.primary.bg_symbol" class="absolute inset-0 w-full h-full z-0" />
 
         <div class="flex flex-col pb-[40px] md:pb-0px]">
           <p class="text-primary uppercase text-base w-[300px] font-bold">
@@ -81,38 +74,27 @@ export default {
             {{ slice.primary.date }}
           </p>
         </div>
-        <div class="md:h-screen relative col-start-6 col-span-12">
-          <div ref="masonry" class="masonry md:grid gap-[20px] relative">
+        <div class="md:min-h-screen md:mb-[300px] relative col-start-6 col-span-12 ">
+          <div ref="masonry" class="masonry flex justify-center md:grid gap-[20px] relative  mx-auto">
             <div class="content text-primary text-download uppercase">
-              <div
-                class="py-20 md:p-20 md:grid grid-cols-2 md:w-[720px] gap-20"
-              >
-                <div
-                  v-for="(el, idx) in slice.primary.card_group"
-                  :key="idx"
-                  class="p-[24px] mb-[20px] md:p-20 bg-white w-full max-w-full md:max-w-[330px] h-auto md:h-[22rem] flex flex-col relative box-agenda"
-                  :class="idx === 1 ? 'mt-20' : ''"
-                  data-item
-                >
-                  <PrismicLink
-                    :field="el?.link"
-                    class="group flex flex-col justify-between h-full"
-                  >
+              <div class="py-[120px] md:p-20  md:grid md:grid-cols-2 md:w-[720px] gap-20 " style="place-self: center;">
+                <div v-for="(el, idx) in slice.primary.card_group" :key="idx"
+                  class="p-[24px] min-w-[328px] max-w-[420px] mb-[20px] md:mb-0 md:p-20 bg-white w-full max-w-full md:max-w-[330px] h-auto md:h-[22rem] flex flex-col relative box-agenda justify-center"
+                  :class="[
+                    el?.xl_font_size
+                      ? 'min-h-[300px] md:min-h-[420px]'
+                      : 'min-h-[164px] md:min-h-[343px]'
+                  ]" data-item>
+                  <PrismicLink :field="el?.link" class="group flex flex-col justify-between h-full">
                     <!-- Hover image -->
-                    <PrismicImage
-                      v-if="el?.hover_image"
-                      :field="el?.hover_image"
-                      class="image-hover-agenda absolute w-full h-full object-cover object-center top-0 left-0 z-[-1]"
-                    />
+                    <PrismicImage v-if="el?.hover_image" :field="el?.hover_image"
+                      class="image-hover-agenda absolute w-full h-full object-cover object-center top-0 left-0 z-[-1]" />
 
                     <span class="block text-[18px]">
                       {{ el.subtitle }}
                     </span>
                     <div class="">
-                      <PrismicRichText
-                        :field="el.paragraph"
-                        :class="el.xl_font_size ? ' text-md' : ' text-base'"
-                      />
+                      <PrismicRichText :field="el.paragraph" :class="el.xl_font_size ? ' text-md' : ' text-base'" />
                     </div>
                   </PrismicLink>
                 </div>
@@ -128,9 +110,11 @@ export default {
 .box-agenda:hover .image-hover-agenda {
   opacity: 1;
 }
+
 .box-agenda:hover {
   color: white;
 }
+
 .image-hover-agenda {
   opacity: 0;
   transition: ease-in 0.5s;
