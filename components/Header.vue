eadedd50bc11c189b8c375e0e04a2140d8e49e87
Julian Peroncini <julianperoncini@gmail.com>
2024-09-06 03:12:17 -0300

    ...

--- components/Header.vue
+++ components/Header.vue
@@ -5,7 +5,7 @@
     class="w-full h-[100px] fixed top-0 z-[1000] md:z-20 md:absolute bg-transparent"
   >
     <div
-      class="flex h-[100px] items-center justify-between items-center px-[16px] md:px-[60px]"
+      class="bg-primary md:bg-transparent flex h-[100px] items-center justify-between px-[16px] md:px-[60px]"
     >
       <LogoWhite />
       <Navigation />
