3358e770864aaf7cdd3ee051d9a61ec6dc96c7fa
Julian Peroncini <julianperoncini@gmail.com>
2024-09-06 03:26:56 -0300

    QA Fixes

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -4,7 +4,6 @@ const { client } = usePrismic();
 const { data: navigation } = await useAsyncData("navigation", () =>
   client.getSingle("navigation")
 );
-
 </script>
 
 <script>
@@ -35,13 +34,12 @@ export default {
       // console.log('lenis instance', lenis);
 
       if (this.isActive) {
-        htmlElement.classList.add('no-scroll');
+        htmlElement.classList.add("no-scroll");
         if (lenis) lenis.stop();
       } else {
-        htmlElement.classList.remove('no-scroll');
+        htmlElement.classList.remove("no-scroll");
         if (lenis) lenis.start();
       }
-
     },
     open() {
       gsap.to(this.$refs.nav, 1.25, {
@@ -103,28 +101,43 @@ export default {
 };
 </script>
 <template>
-  <div ref="nav"
-    class="max-h-[100vh] w-screen max-h-screen !overflow-y-hidden fixed top-0 inset-0 bg-primary bg-radial-gradient flex flex-col justify-between nav z-20">
-
-    <LogoWhite class="absolute left-0 top-[10px] px-[16px] md:px-[60px]" />
+  <div
+    ref="nav"
+    class="max-h-[100vh] w-full max-h-screen !overflow-y-hidden fixed top-0 inset-0 bg-primary bg-radial-gradient flex flex-col justify-between nav z-20"
+  >
+    <LogoWhite
+      class="relative md:absolute left-0 top-[10px] px-[16px] md:px-[60px]"
+    />
 
     <div class="max-w-[900px]">
-      <img class="absolute  md:px-[250px]  top-0 max-w-full w-full h-auto" src="/public/img/menu-ellipse.svg" />
+      <img
+        class="absolute md:px-[250px] top-0 max-w-full w-full h-auto"
+        src="/public/img/menu-ellipse.svg"
+      />
     </div>
 
-
     <div
-      class="flex flex-col py-80 md:flex-row justify-center gap-80  md:justify-between items-center md:items-end lg:items-end h-full flex-end w-full px-10 md:px-60 h-full">
+      class="flex flex-col py-80 md:flex-row justify-center gap-80 md:justify-between items-center md:items-end lg:items-end h-full flex-end w-full px-10 md:px-60 h-full"
+    >
       <!-- Navigation Links Section -->
-      <div class="flex flex-col md:flex-col items-start w-full sm:max-w-[720px]">
-        <div v-for="(item, index) in navigation.data.links_menu" :key="index"
+      <div
+        class="flex flex-col md:flex-col items-start w-full sm:max-w-[720px]"
+      >
+        <div
+          v-for="(item, index) in navigation.data.links_menu"
+          :key="index"
           class="text-white font-display w-[720px] uppercase text-lg md:text-md overflow-hidden relative py-20 md:py-10 last:border-b-[1px] last:border-white last:border-opacity-20 last:pb-30"
-          @click="toggle">
+          @click="toggle"
+        >
           <div ref="navItems">
-            <PrismicLink class="flex items-center border-t-[1px] border-white border-opacity-20 cursor-pointer w-full"
-              :field="item.url_link">
+            <PrismicLink
+              class="flex items-center border-t-[1px] border-white border-opacity-20 cursor-pointer w-full"
+              :field="item.url_link"
+            >
               <span class="font-light mt-40">0{{ index + 1 }}</span>
-              <span class="text-highlight sm:text-titleSection_mb md:text-titleSection_mb pl-20 pt-30">
+              <span
+                class="text-highlight sm:text-titleSection_mb md:text-titleSection_mb pl-20 pt-30"
+              >
                 {{ slugToString(item.label_menu) }}
               </span>
             </PrismicLink>
@@ -133,25 +146,37 @@ export default {
       </div>
 
       <!-- Prismic Links Section -->
-      <div class="flex flex-col md:flex-col space-y-20 z-20 w-full flex-end max-w-[360px]">
+      <div
+        class="flex flex-col md:flex-col space-y-20 z-20 w-full flex-end max-w-[360px]"
+      >
         <PrismicLink :field="navigation.data.pact_link" class=" ">
           <div
-            class="w-full md:w-[360px] h-[140px] py-20 md:py-8 pl-20 xs:pr-20 xs:py-20 sm:py-20 sm:pr-10 md:pr-10 bg-box-bg flex flex-row cursor-pointer justify-between">
+            class="w-full md:w-[360px] h-[140px] py-20 md:py-8 pl-20 xs:pr-20 xs:py-20 sm:py-20 sm:pr-10 md:pr-10 bg-box-bg flex flex-row cursor-pointer justify-between"
+          >
             <p
-              class="text-secondary uppercase w-[150px] pt-40 pl-40 md:pt-20 md:pl-10 font-bold sm:w-[110px] md:w-[140px] sm:leading-20 md:leading-10 z-30">
+              class="text-secondary uppercase w-[150px] pt-40 pl-40 md:pt-20 md:pl-10 font-bold sm:w-[110px] md:w-[140px] sm:leading-20 md:leading-10 z-30"
+            >
               {{ navigation.data.pact_title }}
             </p>
-            <img class="w-[53px] h-[53px] self-end mb-5" src="/public/img/arrow-right-menu.svg">
+            <img
+              class="w-[53px] h-[53px] self-end mb-5"
+              src="/public/img/arrow-right-menu.svg"
+            />
           </div>
         </PrismicLink>
         <PrismicLink :field="navigation.data.sof_link">
           <div
-            class="w-full md:w-[360px] h-[140px] py-20 md:py-8 pl-20 xs:pr-20 xs:py-20 sm:py-20 sm:pr-10 md:pr-10 bg-box-bg flex flex-row cursor-pointer justify-between">
+            class="w-full md:w-[360px] h-[140px] py-20 md:py-8 pl-20 xs:pr-20 xs:py-20 sm:py-20 sm:pr-10 md:pr-10 bg-box-bg flex flex-row cursor-pointer justify-between"
+          >
             <p
-              class="text-secondary uppercase w-[150px] pt-40 pl-40 md:pt-20 md:pl-10 font-bold sm:w-[110px] md:w-[140px] sm:leading-20 md:leading-10 z-30">
+              class="text-secondary uppercase w-[150px] pt-40 pl-40 md:pt-20 md:pl-10 font-bold sm:w-[110px] md:w-[140px] sm:leading-20 md:leading-10 z-30"
+            >
               {{ navigation.data.sof_title }}
             </p>
-            <img class="w-[53px] h-[53px] self-end mb-5" src="/public/img/arrow-right-menu.svg">
+            <img
+              class="w-[53px] h-[53px] self-end mb-5"
+              src="/public/img/arrow-right-menu.svg"
+            />
           </div>
         </PrismicLink>
       </div>
@@ -159,18 +184,21 @@ export default {
   </div>
 
   <div class="flex flex-row gap-20" v-if="$route.path !== '/'">
-    <button ref="toggleNav"
+    <button
+      ref="toggleNav"
       class="w-[50px] h-[50px] flex flex-col border border-[var(--burger-color)] rounded-full items-center justify-center z-20 toggle-nav"
-      :class="{ active: isActive }" @click="toggle">
+      :class="{ active: isActive }"
+      @click="toggle"
+    >
       <div
-        class="w-[20px] h-[2px] bg-[var(--burger-color)] mx-auto origin-left transition-transform duration-700 ease-expo-out">
-      </div>
+        class="w-[20px] h-[2px] bg-[var(--burger-color)] mx-auto origin-left transition-transform duration-700 ease-expo-out"
+      ></div>
       <div
-        class="w-[20px] h-[2px] bg-[var(--burger-color)] my-[4px] mx-auto origin-left transition-transform duration-700 ease-expo-out">
-      </div>
+        class="w-[20px] h-[2px] bg-[var(--burger-color)] my-[4px] mx-auto origin-left transition-transform duration-700 ease-expo-out"
+      ></div>
       <div
-        class="w-[20px] h-[2px] bg-[var(--burger-color)] scale-x-50 origin-left transition-transform duration-700 ease-expo-out">
-      </div>
+        class="w-[20px] h-[2px] bg-[var(--burger-color)] scale-x-50 origin-left transition-transform duration-700 ease-expo-out"
+      ></div>
     </button>
   </div>
 </template>
