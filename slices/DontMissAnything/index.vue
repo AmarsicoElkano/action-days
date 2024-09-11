17d7c77a1e92a0b7520408b29a48b7b8529d6216
Maryam TB <maryamtb.17@gmail.com>
2024-09-11 11:01:35 +0100

    dont miss anything tweaks

--- slices/DontMissAnything/index.vue
+++ slices/DontMissAnything/index.vue
@@ -94,20 +94,20 @@ export default {
 </script>
 
 <template>
-  <section class="bg-light w-full h-auto min-h-[900px] py-[80px] relative text-primary overflow-hidden">
+  <section class="bg-light w-full h-[800px] py-[80px] relative text-primary overflow-hidden">
     <PrismicImage class="absolute top-[-240px] right-0 max-w-[750px] z-0" :field="slice.primary.symbol_top_right" />
-    <PrismicImage class="absolute bottom-[-240px] left-0 max-w-[750px] z-0" :field="slice.primary.symbol_bottom_left" />
+    <PrismicImage class="absolute bottom-[-200px] left-0 max-w-[620px] z-0" :field="slice.primary.symbol_bottom_left" />
     <div class="grid grid-cols-12 md:grid-cols-12 gap-6 z-20 w-full">
       <h1 data-title
-        class="col-span-6 col-start-1 px-40 xs:col-start-2 md:col-start-3 lg:col-start-3 uppercase text-headline_mb pt-[220px] sm:pt-[180px] z-20 max-w-[900px] lg:max-w-[1200px]"
+        class="pl-40 col-span-6 col-start-1 xs:px-0 xs:col-start-2 md:col-start-3 lg:col-start-3 uppercase text-headline_mb pt-[0px] sm:pt-[75px] z-20 max-w-[900px] lg:max-w-[1200px]"
         v-if="slice.primary.title">
-        <span class="pl-60">{{ slice.primary.title.split(" ")[0] }}</span>
+        <span class=" sm:pl-60">{{ slice.primary.title.split(" ")[0] }}</span>
         {{ slice.primary.title.substring(slice.primary.title.indexOf(" ")) }}
       </h1>
     </div>
     <div class="grid grid-cols-12 mt-[80px] md:pt-20 lg:pt-32">
       <div data-item
-        class="col-span-12 z-20 px-60 xs:col-start-2 sm:col-start-6 md:col-start-7 lg:col-start-8 lg:col-span-6 flex flex-col items-start justify-center gap-4 pr-8 md:pr-12 lg:pr-16 js-visit-summit">
+        class="col-span-12 z-20 px-60 sm:px-0 xs:col-start-2 sm:col-start-6 md:col-start-7 lg:col-start-8 lg:col-span-6 flex flex-col items-start justify-center gap-4 pr-8 md:pr-12 lg:pr-16 js-visit-summit">
         <PrismicRichText class="w-full leading-20 pb-10" style="font-size: 20px" :field="slice.primary.description" />
 
         <button
