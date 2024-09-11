1d245e099469fcb51b91c3e4fbc79a40629d37c3
Maryam TB <maryamtb.17@gmail.com>
2024-09-11 14:36:09 +0100

    w&w

--- slices/WhenandWhere/index.vue
+++ slices/WhenandWhere/index.vue
@@ -112,21 +112,19 @@ export default {
 </script>
 
 <template>
-  <section id="whenwhere" :ref="setRef" class="bg-primary w-full min-h-screen relative" data-section="when&where"
+  <section id="whenwhere" :ref="setRef" class="bg-primary w-full max-h-[900px] relative" data-section="when&where"
     data-nav="light">
-    <div>
-      <PrismicImage :field="slice.primary.bg_symbol" class="z-[10] absolute w-full h-full left-0 opacity-0 md:opacity-100" />
-      <div class="flex flex-col md:flex-row gap-100 py-[140px] pl-40 pr-40">
-        <PrismicImage :field="slice.primary.image" class="sm:w-[450px] pt-10" data-image />
-        <div class="z-[20] flex flex-col gap-100 pt-40">
-          <div v-for="(item, index) in slice.primary.questions" :key="index" class="text-secondary ">
-            <h1 data-title class=" uppercase font-bold" style="font-size: 36px;">
-              {{ item.question_title }}
-            </h1>
-            <p data-text class="pl-40 pt-40">
-              {{ item.question_response }}
-            </p>
-          </div>
+    <PrismicImage :field="slice.primary.bg_symbol" class="z-10 absolute w-full top-0 m-auto opacity-0 md:opacity-100" />
+    <div class="flex flex-col md:flex-row gap-100 py-[140px] pl-40 pr-40">
+      <PrismicImage :field="slice.primary.image" class="z-20 xs:max-w-[325px] sm:max-w-[450px] pt-10" data-image />
+      <div class="z-[20] flex flex-col gap-100 pt-40 sm:pb-20">
+        <div v-for="(item, index) in slice.primary.questions" :key="index" class="text-secondary ">
+          <h1 data-title class=" uppercase font-bold" style="font-size: 36px;">
+            {{ item.question_title }}
+          </h1>
+          <p data-text class="pl-40 pt-[40px] text-landing_mb">
+            {{ item.question_response }}
+          </p>
         </div>
       </div>
     </div>
