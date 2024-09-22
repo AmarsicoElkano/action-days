fcef93cbcea0da54c2eaee47a7e7546a0f6c68e4
AleBj <alejandromarsico@gmail.com>
2024-09-22 17:38:06 -0300

    build

--- server/api/prismic-webhook.post.js
+++ server/api/prismic-webhook.post.js
@@ -4,7 +4,6 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   
   // Verify the webhook secret (you should set this in Prismic and your environment variables)
-
   const secret = process.env.PRISMIC_WEBHOOK_SECRET
   if (event.headers.get('prismic-webhook-secret') !== secret) {
     console.log('error:',event.headers.get('prismic-webhook-secret'))
@@ -14,7 +13,6 @@ export default defineEventHandler(async (event) => {
   // Logic to clear the cache
   // This will depend on how you've implemented caching in your Nuxt 3 app
   // For example, if you're using Nitro's built-in caching:
-
   await Promise.all([
     clearNuxtData(),
     clearNuxtDataCache()
@@ -22,7 +20,6 @@ export default defineEventHandler(async (event) => {
   
   // You might also want to regenerate any static pages here
   // This is an example and may need to be adjusted based on your setup
-
   await $fetch('/api/revalidate')
   return { status: 200, body: 'Cache cleared successfully' }
 })
